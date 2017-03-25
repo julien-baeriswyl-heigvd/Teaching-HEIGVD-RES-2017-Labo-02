fa8a560a4fb5d0a6968ec03d08ef5b9fb6fc9a22
Julien Baeriswyl <julien.baeriswyl@heig-vd.ch>
2017-03-25 12:23:35 +0100

    Almost ready implementation v1

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
@@ -6,65 +6,193 @@ import ch.heigvd.res.labs.roulette.net.protocol.RouletteV1Protocol;
 import ch.heigvd.res.labs.roulette.data.Student;
 import ch.heigvd.res.labs.roulette.net.protocol.InfoCommandResponse;
 import ch.heigvd.res.labs.roulette.net.protocol.RandomCommandResponse;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+
+import java.io.*;
+import java.lang.reflect.Array;
 import java.net.Socket;
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collector;
 
 /**
  * This class implements the client side of the protocol specification (version 1).
  * 
  * @author Olivier Liechti
  */
-public class RouletteV1ClientImpl implements IRouletteV1Client {
+public class RouletteV1ClientImpl implements IRouletteV1Client
+{
+    private enum CommandNumber
+    {
+        HELP,
+        RANDOM,
+        LOAD,
+        INFO,
+        BYE
+    }
 
-  private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
+    private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
 
-  @Override
-  public void connect(String server, int port) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    private Socket         clientSocket;
+    private PrintWriter    pw;
+    private BufferedReader br;
+    private String         answer;
 
-  @Override
-  public void disconnect() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    /**
+     *
+     * @param nr
+     * @return
+     * @throws IOException
+     */
+    private boolean sendCommand (CommandNumber nr) throws IOException
+    {
+        // JBL: ?
+        if (clientSocket != null)
+        {
+            pw.println(RouletteV1Protocol.SUPPORTED_COMMANDS[nr.ordinal()]);
 
-  @Override
-  public boolean isConnected() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+            if (pw.checkError())
+            {
+                throw new IOException("failed to send command - `" + RouletteV1Protocol.SUPPORTED_COMMANDS[nr.ordinal()] + "`");
+            }
 
-  @Override
-  public void loadStudent(String fullname) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+            switch(nr)
+            {
+                case HELP:
+                case INFO:
+                case RANDOM:
+                case LOAD:
+                    answer = br.readLine();
+                    return !answer.isEmpty();
+                case BYE:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        return false;
+    }
 
-  @Override
-  public void loadStudents(List<Student> students) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    /**
+     *
+     * @param data
+     * @return
+     */
+    private boolean sendData (String data) throws IOException
+    {
+        if (clientSocket != null)
+        {
+            pw.println(data);
+            if (pw.checkError())
+            {
+                throw new IOException("failed to send data");
+            }
 
-  @Override
-  public Student pickRandomStudent() throws EmptyStoreException, IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+            pw.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
+            if (pw.checkError())
+            {
+                throw new IOException("failed to send EOT");
+            }
 
-  @Override
-  public int getNumberOfStudents() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+            return br.readLine().equals(RouletteV1Protocol.RESPONSE_LOAD_DONE);
+        }
 
-  @Override
-  public String getProtocolVersion() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+        return false;
+    }
 
+    @Override
+    public void connect(String server, int port) throws IOException
+    {
+        // JBL: ?
+        clientSocket = new Socket(server, port);
+        if (isConnected())
+        {
+            pw = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        }
+    }
 
+    @Override
+    public void disconnect() throws IOException
+    {
+        // JBL: ?
+        if (sendCommand(CommandNumber.BYE))
+        {
+            clientSocket.close();
+            clientSocket = null;
+            pw = null;
+            br = null;
+        }
+    }
 
+    @Override
+    public boolean isConnected()
+    {
+        // JBL: ?
+        return clientSocket.isConnected();
+    }
+
+    @Override
+    public void loadStudent(String fullname) throws IOException
+    {
+        // JBL: ?
+        if (!sendCommand(CommandNumber.LOAD))
+        {
+            throw new IOException("failed to launch LOAD operation");
+        }
+
+        if (!sendData(fullname))
+        {
+
+        }
+    }
+
+    @Override
+    public void loadStudents(List<Student> students) throws IOException
+    {
+        final String SEPARATOR = String.format("%n");
+
+        // JBL: ?
+        if (!sendCommand(CommandNumber.LOAD))
+        {
+            throw new IOException("failed to launch LOAD operation");
+        }
+    }
+
+    @Override
+    public Student pickRandomStudent() throws EmptyStoreException, IOException
+    {
+        // JBL: ?
+        if (!sendCommand(CommandNumber.RANDOM))
+        {
+            throw new IOException("failed to retrieve student");
+        }
+
+        return Student.fromJson(answer);
+    }
+
+    @Override
+    public int getNumberOfStudents() throws IOException
+    {
+        // JBL: ?
+        if(!sendCommand(CommandNumber.INFO))
+        {
+            throw new IOException("failed to retrieve global information");
+        }
+
+        return JsonObjectMapper.parseJson(answer, InfoCommandResponse.class).getNumberOfStudents();
+    }
+
+    @Override
+    public String getProtocolVersion() throws IOException
+    {
+        // JBL: ?
+        if(!sendCommand(CommandNumber.INFO))
+        {
+            throw new IOException("failed to retrieve global information");
+        }
+
+        return JsonObjectMapper.parseJson(answer, InfoCommandResponse.class).getProtocolVersion();
+    }
 }
