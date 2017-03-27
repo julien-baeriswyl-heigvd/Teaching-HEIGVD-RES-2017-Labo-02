a07488198f0fc55d309892be23b0f28cb8bb0d36
Julien Baeriswyl <julien.baeriswyl@heig-vd.ch>
2017-03-27 12:01:27 +0200

    Temp version 2, not finished

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
@@ -6,15 +6,26 @@ import ch.heigvd.res.labs.roulette.data.StudentsList;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV1Protocol;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * This class implements the client side of the protocol specification (version 2).
  *
  * @author Olivier Liechti
+ * @author Julien Baeriswyl   [MODIFIED BY] (julien.baeriswyl@heig-vd.ch)
+ * @author Iando Rafidimalala [MODIFIED BY] (iando.rafidimalala@heig-vd.ch)
  */
 public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRouletteV2Client
 {
+    @Override
+    protected String[] getSupportedCommands ()
+    {
+        return RouletteV2Protocol.SUPPORTED_COMMANDS;
+    }
+
     @Override
     public void clearDataStore() throws IOException
     {
@@ -23,7 +34,7 @@ public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRoule
             throw new IOException("failed to send clear command");
         }
 
-        checkAnswer(RouletteV2Protocol.CMD_CLEAR);
+        retrieveAnswer(RouletteV2Protocol.CMD_CLEAR);
         if (!getAnswer().equals(RouletteV2Protocol.RESPONSE_CLEAR_DONE))
         {
             throw new IOException("failed to clear students list");
@@ -38,6 +49,8 @@ public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRoule
             throw new IOException("failed to ask students list");
         }
 
-        checkAnswer(RouletteV2Protocol.CMD_LIST);
+        retrieveAnswer(RouletteV2Protocol.CMD_LIST);
+
+        return JsonObjectMapper.parseJson(getAnswer(), StudentsList.class).getStudents();
     }
 }
--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
@@ -5,20 +5,82 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  * This class implements the Roulette protocol (version 2).
  *
  * @author Olivier Liechti
+ * @author Julien Baeriswyl   [MODIFIED BY] (julien.baeriswyl@heig-vd.ch)
+ * @author Iando Rafidimalala [MODIFIED BY] (iando.rafidimalala@heig-vd.ch)
  */
-public class RouletteV2ClientHandler implements IClientHandler {
+public class RouletteV2ClientHandler implements IClientHandler
+{
+    final static Logger LOG = Logger.getLogger(RouletteV1ClientHandler.class.getName());
 
-  public RouletteV2ClientHandler(IStudentsStore store) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of gen  
-  }
+    private final IStudentsStore store;
 
-  @Override
-  public void handleClientConnection(InputStream is, OutputStream os) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    public RouletteV2ClientHandler(IStudentsStore store)
+    {
+        this.store = store;
+    }
 
+    @Override
+    public void handleClientConnection(InputStream is, OutputStream os) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        PrintWriter    writer = new PrintWriter(new OutputStreamWriter(os));
+
+        writer.println("Hello. Online HELP is available. Will you find it?");
+        writer.flush();
+
+        String command;
+        boolean done = false;
+        while (!done && ((command = reader.readLine()) != null)) {
+            LOG.log(Level.INFO, "COMMAND: {0}", command);
+            switch (command.toUpperCase()) {
+                case RouletteV2Protocol.CMD_RANDOM:
+                    RandomCommandResponse rcResponse = new RandomCommandResponse();
+                    try {
+                        rcResponse.setFullname(store.pickRandomStudent().getFullname());
+                    } catch (EmptyStoreException ex) {
+                        rcResponse.setError("There is no student, you cannot pick a random one");
+                    }
+                    writer.println(JsonObjectMapper.toJson(rcResponse));
+                    writer.flush();
+                    break;
+                case RouletteV2Protocol.CMD_HELP:
+                    writer.println("Commands: " + Arrays.toString(RouletteV1Protocol.SUPPORTED_COMMANDS));
+                    break;
+                case RouletteV2Protocol.CMD_INFO:
+                    InfoCommandResponse response = new InfoCommandResponse(RouletteV1Protocol.VERSION, store.getNumberOfStudents());
+                    writer.println(JsonObjectMapper.toJson(response));
+                    writer.flush();
+                    break;
+                case RouletteV2Protocol.CMD_LOAD:
+                    writer.println(RouletteV2Protocol.RESPONSE_LOAD_START);
+                    writer.flush();
+                    store.importData(reader);
+                    writer.println(RouletteV2Protocol.RESPONSE_LOAD_DONE);
+                    writer.flush();
+                    break;
+                case RouletteV2Protocol.CMD_BYE:
+                    done = true;
+                    break;
+                default:
+                    writer.println("Huh? please use HELP if you don't know what commands are available.");
+                    writer.flush();
+                    break;
+            }
+            writer.flush();
+        }
+
+    }
 }
